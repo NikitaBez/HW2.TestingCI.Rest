26b7d51e642b460dae1cf4a4bad4b3f6577434db
NikitaBez <bezmenov.nikita@gmail.com>
2022-10-09 22:12:37 +0900

    Update MobileBankApiTestV3

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -5,6 +5,7 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV3 {
@@ -21,10 +22,13 @@ class MobileBankApiTestV3 {
       .then()
           .statusCode(200)
           // специализированные проверки - лучше
-          .contentType(ContentType.JSON)
-          .body("", hasSize(3))
+//          .contentType(ContentType.JSON)
+//          .body("", hasSize(3))
           .body("[0].currency", equalTo("RUB"))
-          .body("[0].balance", greaterThanOrEqualTo(0))
+          .body("[1].currency", equalTo("USD"))
+          .body("[2].currency", equalTo("RUB"))
+          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+//          .body("[0].balance", greaterThanOrEqualTo(0))
       ;
     }
 }
